3c13947c03e1f9ac5fa92e1c3f4782a84a15f4ea
diyadave <diyadave26@gmail.com>
2026-02-06 20:38:04 +0530

    Update google_sheet.py

--- app/google_sheet.py
+++ app/google_sheet.py
@@ -1,18 +1,24 @@
 import gspread
-from google.oauth2.service_account import Credentials
 import os
+import json
+from google.oauth2.service_account import Credentials
 
 def send_to_sheet(name, email, subject, message):
-    creds_file = os.getenv("GOOGLE_SERVICE_ACCOUNT_FILE")
+    creds_json = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON")
     sheet_id = os.getenv("GOOGLE_SHEET_ID")
+
+    if not creds_json:
+        raise Exception("GOOGLE_SERVICE_ACCOUNT_JSON not set")
+
+    creds_info = json.loads(creds_json)
 
     scopes = [
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive"
     ]
 
-    creds = Credentials.from_service_account_file(
-        creds_file,
+    creds = Credentials.from_service_account_info(
+        creds_info,
         scopes=scopes
     )
 
