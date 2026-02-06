20c287155013ae017f393fb4115dc05ffa862818
diyadave <diyadave26@gmail.com>
2026-02-06 19:04:38 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
         "http://127.0.0.1:5501",
         "http://localhost:5500",
         "http://localhost:5501",
+        "https://magical-pasca-09f99a.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
